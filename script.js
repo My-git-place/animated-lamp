69093da318fbb7e25fc9c1e5738e2430371573b9
My-git-place <79379987+My-git-place@users.noreply.github.com>
2021-02-21 10:55:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var headingEl = document.getElementById("head");
+var leftEl = document.getElementById("left_arrow");
+var rightEl = document.getElementById("right_arrow");
+var slide = 1;
+var onLeftClick = function(){
+slide=slide-1;
+}
+var onRightClick = function(){
+slide=slide+1;
+}
+leftEl.addEventListener("click",onLeftClick);
+rightEl.addEventListener("click",onRightClick);
+if (slide===1){
+headingEl.innerHTML="Owen";
+} else if (slide===2){
+headingEl.innerHTML="Meeghan";
+} else if (slide===3){
+headingEl.innerHTML="Mihir";
+} else if (slide===4){
+headingEl.innerHTML="Brigita";
+}
